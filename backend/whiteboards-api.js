454e3e1cabc5d6680c953f35c14798d00ec92dbc
LethalPineapple <justin_zhang02@outlook.com>
2025-09-10 11:06:17 -0400

    I re added model

--- backend/whiteboards-api.js
+++ backend/whiteboards-api.js
@@ -1,10 +1,33 @@
-import express from "express";
-import { ObjectId } from "mongodb";
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://originalpineapple02:<db_password>@cluster0.3op48.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri);
 
-export function createWhiteboardRouter(db) {
-  const router = express.Router();
-  const whiteboards = db.collection("whiteboards");
-
-
-
+async function getWB(id) {
+  try {
+    const database = client.db('idea-pile-db');
+    const whiteboards = database.collection('whiteboards');
+    // Queries for a movie that has a title value of 'Back to the Future'
+    const query = { _id: new ObjectId(id) };
+    const whiteboard = await whiteboards.findOne(query);
+    console.log(whiteboard);
+  } finally {
+    await client.close();
+  }
 }
+
+async function getUserWB(userID) {
+  try {
+    const database = client.db('idea-pile-db');
+    const whiteboards = database.collection('whiteboards');
+    // Queries for a movie that has a title value of 'Back to the Future'
+    const query = { _UID: userID };
+    const whiteboard = await whiteboards.find(query).toArray();
+    console.log(whiteboard);
+  } finally {
+    await client.close();
+  }
+
+  
+}
+
